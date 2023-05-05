9014155841463ffe08b42b1cce21084e72e4a135
criskip <chriskipkemboi3@gmail.com>
2023-05-05 13:31:37 +0300

    Added some items to project

--- app/src/main/java/com/example/firecloudchat_app/Launcher.kt
+++ app/src/main/java/com/example/firecloudchat_app/Launcher.kt
@@ -1,11 +1,32 @@
 package com.example.firecloudchat_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Launcher : AppCompatActivity() {
+
+    private lateinit var txt_welcome:TextView
+    private lateinit var txt_about:TextView
+    private lateinit var txt_thanks:TextView
+    private lateinit var btn_proceed:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
+
+        txt_welcome = findViewById(R.id.mtxtWelcom)
+        txt_about = findViewById(R.id.mtxtAbout)
+        txt_thanks = findViewById(R.id.mtxtThanks)
+        btn_proceed = findViewById(R.id.mbtnProceed)
+
+
+        btn_proceed.setOnClickListener {
+            var gotologin = Intent(this,LoginActivity::class.java)
+            startActivity(gotologin)
+
+        }
+
     }
 }
